4ad4a6c399fd18420a70374144080367f88b800d
ramsai949 <138433609+ramsai949@users.noreply.github.com>
2023-12-07 10:34:48 +0530

    Add files via upload

---
+++ .py
@@ -0,0 +1,39 @@
+# class Employee:
+#     def __init__(self,ename,esal,eno):
+#         self.ename=ename
+#         self.esal=esal
+#         self.eno=eno
+#     def Display(self):
+#         print("Employee name: ",self.ename)
+#         print("Employee esal: ",self.esal)
+#         print("Employee eno: ",self.eno)
+
+# class Test:
+#     def modify(emp):
+#         emp.esal+=50
+#         emp.Display()
+# e =Employee ("sai",25000,45)
+# Test.modify(e)
+
+
+
+# class Student:   
+#     def __init__(self,sname,sfee,sphn):
+#         self.sname=sname
+#         self.sfee=sfee
+#         self.sphn=sphn
+#     def say(self):
+#         print(self.sname)
+#         print(self.sfee)
+#         print(self.sphn)
+        
+# class Sai:
+#     def modify(stu):
+#         stu.sname+='sai'
+#         stu.say()
+# s = Student("ram",15000,9381)
+# Sai.modify(s)
+
+
+
+
